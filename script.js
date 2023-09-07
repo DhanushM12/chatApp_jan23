f955d1a6405eef4881ef0bcd0b830a32845436f8
DhanushM12 <dhanushmukhi@gmail.com>
2023-09-07 21:10:03 +0530

    chat app

--- script.js
+++ script.js
@@ -5,18 +5,29 @@ const messageForm = document.getElementById('send-container');
 const messageInput = document.getElementById('message-input');
 
 let name = prompt('What is your name')
-console.log(name)
+// console.log(name)
+appendMessage("You Joined")
 
 socket.emit('new-user', name);
 
 socket.on('user-connected', name => {
+    appendMessage(`${name} connected`)
+})
 
+socket.on('chat-message', data => {
+    appendMessage(`${data.name} : ${data.message}`)
+})
+
+socket.on('user-disconnected', name => {
+    appendMessage(`${name} disconnected`)
 })
 
 messageForm.addEventListener('submit', e=> {
     e.preventDefault();
     const message = messageInput.value;
     appendMessage(`You : ${message}`);
+    socket.emit('send-chat-message', message);
+    messageInput.value = '';
 })
 
 function appendMessage(message) {
